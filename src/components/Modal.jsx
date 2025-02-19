e730d121608d62572e330ff3d662519b7da8e566
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2025-02-19 14:51:37 +0200

    made modals responsive

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+// import React from 'react';
+
+// const Modal = ({ isOpen, onClose, children }) => {
+
+//   if (!isOpen) return null;
+
+//   return (
+//     <div className="modal-overlay" onClick={onClose}>
+//       <div className="modal-content flex flex-col justify-center items-center p-4 md:p-6" onClick={e => e.stopPropagation()}>
+//         <button className="close-button absolute top-4 right-4 w-6 h-6 md:w-8 md:h-8 text-2xl md:text-3xl" onClick={onClose}>&times;</button>
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Modal;
+import React from "react";
 
 const Modal = ({ isOpen, onClose, children }) => {
-
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content flex flex-col justify-center items-center p-4 md:p-6" onClick={e => e.stopPropagation()}>
-        <button className="close-button absolute top-4 right-4 w-6 h-6 md:w-8 md:h-8 text-2xl md:text-3xl" onClick={onClose}>&times;</button>
+    <div
+      className="fixed inset-0 bg-gray-200 bg-opacity-10 flex justify-center items-center p-4"
+      onClick={onClose}
+    >
+      <div
+        className="relative w-full max-w-md bg-white p-6 rounded-lg shadow-lg max-h-[90vh] overflow-y-auto"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* Close Button */}
+        <button
+          className="absolute top-4 right-4 text-gray-700 hover:text-gray-900 text-2xl p-2"
+          onClick={onClose}
+        >
+          &times;
+        </button>
         {children}
       </div>
     </div>
--- src/components/WhatsappBooking.jsx
+++ src/components/WhatsappBooking.jsx
@@ -13,7 +13,7 @@ const WhatsappBooking = () => {
   const [formData, setFormData] = useState({
     name: '',
     type: '',
-    date: 'Saturday'
+    date: ''
   });
 
   const handleInputChange = (e) => {
@@ -107,8 +107,8 @@ const WhatsappBooking = () => {
         </div>
       </div>
 
-      <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"> {/* Submit button styling */}
-        Submit
+      <button type="submit" className="w-full bg-green-400 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"> {/* Submit button styling */}
+        Send Message
       </button>
     </form>
   );
